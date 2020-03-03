3676232a7cd177c663055bc6b683434779d803f8
Richard Bobo <richard.bobo@gmail.com>
2020-03-03 18:26:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -56,10 +56,28 @@ end
 
 def matrix_lookup(matrix, row, column)
   
-  CONVENTIONAL_PRODUCE.sort  
+  product_storage_room = [
+    
+[
+  "Strawberries",
+  "Potatoes",
+  "Grapes",
+  "Avocadoes",
+  "Asparagus"
+]
+
+ [
+  "Grapefruit",
+  "Pineapple",
+  "Oranges",
+  "Watermelon",
+  "Eggplant"
+]
+    
+    
+    ]
   
-  assembled_matrix[product_storage_room][1][1] 
-   sorted_matrix[1][1]
+
   
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
